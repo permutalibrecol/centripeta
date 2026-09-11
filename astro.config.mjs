287ef3f3137dec7015e37d259b6f80798ebc222d
permutalibrecol <permutalibre@gmail.com>
2026-09-11 04:05:48 -0500

    Fix site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  // Configuración estándar y limpia para tu proyecto
-  site: 'https:/permutalibrecol/github.io',
+  site: 'https://permutalibrecol.github.io',
   base: '/centripeta',
   output: 'static'
 });
